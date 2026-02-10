2992a6ea553eeea77aad857a3577a47a536c7eb5
DynorBlue <dynorblue@gmail.com>
2026-02-09 19:22:39 -0600

    creacion de la capeta del proyecto

---
+++ CleanDataApi/src/main/java/utl/org/ldsm504/sakura/CleanDataApi/CleanDataApiApplication.java
@@ -0,0 +1,13 @@
+package utl.org.ldsm504.sakura.CleanDataApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanDataApiApplication.class, args);
+	}
+
+}
---
+++ CleanDataApi/src/test/java/utl/org/ldsm504/sakura/CleanDataApi/CleanDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package utl.org.ldsm504.sakura.CleanDataApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
